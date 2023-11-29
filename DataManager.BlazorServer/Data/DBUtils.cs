67ae1216de078ed354e9b3125949e2725b313ec6
GS_ZJ <295737015@qq.com>
2023-11-29 17:23:36 +0800

    1.增加数据数据查询业务 2.初步完成Web API业务

--- DataManager.BlazorServer/Data/DBUtils.cs
+++ DataManager.BlazorServer/Data/DBUtils.cs
@@ -21,16 +21,5 @@ namespace DataManager.BlazorServer.Data
             }
             return new StandDataList(entity);
         }
-
-        public static StandDataList Inquire(IEnumerable<IFilterAction> filter)
-        {
-            //SqliteDatabase.GetInstance().Inquire("");
-            //using (MyDbContext context = new MyDbContext())
-            //{
-            //    var ret = context.SpectrumItems.Select(t => new SpectrumInquireItem(t)).Where(filter.GetFilterFunc<SpectrumInquireItem>()).ToList();
-            //    return ret;
-            //}
-            return null;
-        }
     }
 }
---
+++ DataManager.Database/Utils/DBUtils.cs
@@ -0,0 +1,24 @@
+﻿using DataManager.Database;
+using DataManager.Database.Data;
+
+namespace DataManager.Database.Utils
+{
+    public class DBUtils
+    {
+        public static StandDataList Inquire(string tableName)
+        {
+            var entity=SqliteDatabase.GetInstance().Inquire("IngredientTbl_192_168_1_4New").Select(t=>t.ToStandardData()).ToList();
+            return new StandDataList(entity);
+        }
+
+        //public static StandDataList Inquire(StandardDataSearchModel model)
+        //{
+        //    var entity = SqliteDatabase.GetInstance().Inquire(model.WorkPosition).Select(t => t.ToStandardData()).ToList();
+        //    if(model.SearchDate?.NullStart!=null&& model.SearchDate?.NullEnd != null)
+        //    {
+        //        entity = entity.Where(t => t.DateTime >= model.SearchDate.Start && t.DateTime <= model.SearchDate.End).ToList();
+        //    }
+        //    return new StandDataList(entity);
+        //}
+    }
+}
